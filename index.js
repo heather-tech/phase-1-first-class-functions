1967f535e5e2e0d7c9ce960ebf5a0f0b21e3cda1
heather kruszewski <heather.k.tech@gmail.com>
2023-01-06 15:16:09 -0500

    commit tree 3ed79f4543b46f424688a25be7eca3e501bf209b

---
+++ index.js
@@ -0,0 +1,30 @@
+// The receivesAFunction function should:
+
+// take a callback function as an argument
+// call the callback function
+// it doesn't matter what this function returns, so long as it calls the callback function
+// The returnsANamedFunction function should:
+
+// take no arguments
+// return a named function
+// The returnsAnAnonymousFunction function should:
+
+// take no arguments
+// return an anonymous function
+
+function receivesAFunction(callback) {
+    callback()
+  }
+  
+  function returnsANamedFunction() {
+    return function named() {
+      console.log("The girl is Arya.")
+    }
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      console.log('The girl has no name.')
+    }
+  }
+  
